9986ec4a71783cd25da1cac453a0c12c1ae17e1b
DannyMcwaves <dannymcwaves96@rocketmail.com>
2016-12-31 09:57:27 +0000

    the main javascript entry

---
+++ dev/js/index.js
@@ -0,0 +1,20 @@
+
+// css dependencies
+import "../css/font-awesome.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/css/bootstrap-flex.min.css";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+
+// js dependencies
+import "core-js/client/shim.min.js";
+import "zone.js/dist/zone.js";
+import "reflect-metadata/Reflect.js";
+import $ from "jquery";
+window.jQuery = window.$ = $;
+import Tether from "tether";
+window.Tether = Tether;
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./util.js";
+
+$("h1").Height();
